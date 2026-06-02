069972e08b9ff335525ae7facb8563cbb8baa3ea
Luca Codorean <codoreanluca@gmail.com>
2026-06-02 17:55:26 +0300

    feat: seed stock detail data

--- backend/app/database/seeders/__init__.py
+++ backend/app/database/seeders/__init__.py
@@ -1,3 +1,4 @@
+from app.database.seeders.stock_detail import StockDetailSeeder
 from app.database.seeders.supported_stocks import SupportedStocksSeeder
 
-SEEDERS = [SupportedStocksSeeder()]
+SEEDERS = [SupportedStocksSeeder(), StockDetailSeeder()]
